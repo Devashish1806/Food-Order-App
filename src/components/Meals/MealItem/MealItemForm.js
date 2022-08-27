d8e198bc8828819a2fdcf4d79fcb07f596946944
Devashish Mali <devashish.mali06@gmail.com>
2022-08-27 20:06:42 +0530

    meals added

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,30 @@
+// import { useState } from 'react';
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  // const [itemCount, setItemCount] = useState(0);
+  // const countHandler = (event) => {
+  //   setItemCount(event.target.value);
+  //   console.log(event.target.value);
+  // };
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+          // onChange: countHandler,
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food Delivered To You</h2>
+      <p>Please have a look on it.</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
